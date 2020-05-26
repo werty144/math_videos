7aead42431f9552e5acbe4668f23f62baeadb0a1
Anton144 <anton-paramonov2000@yandex.ru>
2020-05-26 21:02:37 +0300

    0 scene done

---
+++ arithmetic_basics/natural_sum/src/0_scene.py
@@ -0,0 +1,74 @@
+from src.functions import *
+
+
+def get_to_dot_vector(group, bullet_list, dot_ind):
+    dot = bullet_list.submobjects[dot_ind]
+    return dot.get_left() - group.get_left() + RIGHT * MED_LARGE_BUFF
+
+
+class Scene0(Scene):
+    def construct(self):
+        make_screen_frame(self)
+
+        bullet_list = My_bullet_list(6)
+        bullet_list.align_on_border(UL)
+        bullet_list.shift(DOWN * 0.3)
+
+        peremena_mest = TextMobject('$x$', ' $+$ ', '$y$', ' $=$ ', '$x$', ' $+$ ', '$y$', arg_separator='')
+        q1 = overscribe_text(peremena_mest.submobjects[3], '?')
+        peremena_mest.set_color_by_tex_to_color_map({'x': RED, 'y': BLUE})
+
+        self.play(FadeIn(peremena_mest))
+        self.play(*swap_variables_animation(4, 6, peremena_mest))
+        self.play(FadeIn(q1))
+        self.play(get_move_group_animation(Group(peremena_mest, q1), get_to_dot_vector(peremena_mest, bullet_list, 0)),
+                  bullet_list.fade_in_dot_animation(0))
+
+        zero_division = TextMobject('$\\frac{1}{0}$ ', '\\xmark').scale(1.3)
+        zero_division.set_color_by_tex_to_color_map({'xmark': "#cc3333"})
+        self.play(FadeIn(zero_division))
+        self.play(get_move_group_animation(zero_division, get_to_dot_vector(zero_division, bullet_list, 1)),
+                  bullet_list.fade_in_dot_animation(1))
+
+        minus_on_minus = TextMobject('(', '$-$', ')',  ' $\\ast$ ', '(', '$-$', ')', ' $=$ ', '$+$', arg_separator='')
+        q2 = overscribe_text(minus_on_minus.submobjects[7], '?')
+        minus_on_minus.set_color_by_tex_to_color_map({'-': BLUE, '+': RED})
+
+        self.play(FadeIn(minus_on_minus))
+        self.play(FadeIn(q2))
+        self.play(
+            get_move_group_animation(Group(minus_on_minus, q2), get_to_dot_vector(minus_on_minus, bullet_list, 2)),
+            bullet_list.fade_in_dot_animation(2))
+
+        frac_eq = TextMobject(':',  '$\\frac{a}{b}$', '$=$', '$\\ast$', '$\\frac{b}{a}$')
+        # frac_eq.set_color_by_tex_to_color_map({'{a}{b}': RED, '{b}{a}': BLUE})
+        q3 = overscribe_text(frac_eq.submobjects[2], '?')
+        self.play(FadeIn(frac_eq))
+        self.play(FadeIn(q3))
+        self.play(
+            get_move_group_animation(Group(frac_eq, q3), get_to_dot_vector(frac_eq, bullet_list, 3)),
+            bullet_list.fade_in_dot_animation(3))
+
+        irr_pow = TextMobject('$5^{\\sqrt{2}}$ $=$ ',
+                              '$\\underbrace{5 \\cdot 5 \\cdot \\ldots \\cdot 5}_{\\sqrt{2}?}$', arg_separator='')
+        self.play(FadeIn(irr_pow.submobjects[0]))
+        self.play(FadeIn(irr_pow.submobjects[1]))
+        self.play(get_move_group_animation(irr_pow, get_to_dot_vector(irr_pow.submobjects[0].submobjects[0], bullet_list, 4)),
+                  bullet_list.fade_in_dot_animation(4))
+
+        neg_root = TextMobject('$\\sqrt{-1}$', '$=$')
+        q4 = overscribe_text(neg_root.submobjects[1], '?')
+        neg_root.submobjects[0].submobjects[2].set_color(BLUE)
+        self.play(FadeIn(neg_root))
+        self.play(FadeIn(q4))
+        self.play(get_move_group_animation(Group(neg_root, q4), get_to_dot_vector(neg_root, bullet_list, 5)),
+                  bullet_list.fade_in_dot_animation(5))
+
+        quote = TextMobject('«Бог создал натуральные числа,\\\\ всё остальное – дело рук человека».\\\\')
+        author = TextMobject('\\copyright \\space Л. Кронекер')
+        quote.shift(RIGHT * ((FRAME_X_RADIUS + irr_pow.get_right()[0])/2 - quote.get_center()[0]), UP * 0.7)
+        author.shift(quote.get_bottom() - author.get_top(), DOWN * 0.3)
+        self.play(Write(quote))
+        self.play(FadeIn(author))
+
+        self.wait()
